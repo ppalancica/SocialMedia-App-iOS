a128cfa97093c3b3a8c1d6119ef7a829146bf837
ppalancica <pavel.palancica@gmail.com>
2022-10-01 22:46:01 +0300

    First commit. Created iOS App project in Xcode

---
+++ SocialMedia-App-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMedia-App-iOS
+//
+//  Created by Pavel Palancica on 10/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
